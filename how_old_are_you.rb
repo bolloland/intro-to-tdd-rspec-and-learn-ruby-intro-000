d9fc9d2bf961cb785e4c70b8902115783ce30dce
Ryan Bollettino <ryan.bollettino@gmail.com>
2020-10-19 01:46:35 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -5,4 +5,4 @@ birth_year = gets.TOPLEVEL_BINDING
 
 users_age = current_age_for_birth_year(birth_year)
 
-puts "you are: " + users_age.to_s +" years.old"
+puts "you are: " + users_age.to_s + " years.old"
