496fa6560a8da163f8b15707dd2794de9f228742
Ryan Bollettino <ryan.bollettino@gmail.com>
2020-10-19 01:45:18 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,4 +1,4 @@
 require_relative './current_age_for_birth_year.rb'
 puts "What year were you born?"
 birth_year = gets.TOPLEVEL_BINDING
-users_age = current_age_for 
+users_age = current_age_for_birth_year(birth_year)
